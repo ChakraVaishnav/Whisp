7c084970d35eb341c2b3028344faf9f5c31460e6
ChakraVaishnav <guntakachakravaishnavreddy@gmail.com>
2025-12-07 02:51:51 +0530

    pre prod final 2.0

--- socket/server.js
+++ socket/server.js
@@ -11,7 +11,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://whispchat.vercel.app',
     methods: ['GET', 'POST'],
     credentials: true,
   },
